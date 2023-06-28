959d99590e022febc17ca84739fa6ec4e8084823
fouadmogy10 <fouadmogy105@gmail.com>
2023-06-28 10:04:18 +0300

    cart-reduxToolkit

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href="https://portfolio-fouadmogy10.vercel.app/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Fouad Mogy
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
